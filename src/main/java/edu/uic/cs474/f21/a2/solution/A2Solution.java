0c8a21d9d9b16e813df73aa3d9e86358ded53d83
makhariaaayush <63086244+makhariaaayush@users.noreply.github.com>
2021-09-23 11:37:27 -0500

    Test 1 Done

--- src/main/java/edu/uic/cs474/f21/a2/solution/A2Solution.java
+++ src/main/java/edu/uic/cs474/f21/a2/solution/A2Solution.java
@@ -1,12 +1,33 @@
 package edu.uic.cs474.f21.a2.solution;
 import edu.uic.cs474.f21.a2.ObjectInspector;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.Map;
 
 public class A2Solution implements ObjectInspector{
     @Override
     public Map<String, String> describeObject(Object o) {
-        throw new Error("Not Implemented");
+        Class<?> c = o.getClass();
+        Field[] fs = c.getDeclaredFields();
+
+        Map<String, String> ret = new HashMap<>();
+
+        for(Field f: fs){
+            try {
+                String key = f.getName();
+                Object valueOfTheField = f.get(o);
+                String value = (String) valueOfTheField;
+                ret.put(key , value);
+            }
+            catch(ReflectiveOperationException e)
+            {
+                throw new Error(e);
+            }
+
+        }
+        return ret;
+
     }
 
     @Override
